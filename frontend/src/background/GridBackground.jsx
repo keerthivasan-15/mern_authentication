571f77c56311d15931de1d443b80debea19da0a3
keerthivasan-15 <keerthivasan0016@gmail.com>
2024-11-11 18:31:55 +0530

    modified background

--- frontend/src/background/GridBackground.jsx
+++ frontend/src/background/GridBackground.jsx
@@ -4,7 +4,6 @@ export function GridBackground({children}) {
     return (
         <div className="min-h-screen w-full overflow-hidden dark:bg-black bg-white dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
             {/* Radial gradient for the container to give a faded look */}
-            <div className="absolute inset-0 pointer-events-none flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
             {children}
         </div>
     );
